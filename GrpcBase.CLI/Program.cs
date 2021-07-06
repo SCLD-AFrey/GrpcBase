8079a7330201296695948ba153bbac3f37dd1233
Arthur Frey <afrey@Steelcloud.com>
2021-07-06 11:48:39 -0400

    Working Version

---
+++ GrpcBase.CLI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcBase.Common;
+
+namespace GrpcBase.CLI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Broadcaster.BroadcasterClient(channel);
+            
+            
+            
+            var input = System.Console.ReadLine();
+            
+            
+            while (input.ToLower() != "exit")
+            {
+
+                var request = new BroadcastRequest()
+                {
+                    Content = input
+                };
+                var reply = await ProcessRequest(request, client);
+                
+                System.Console.WriteLine(reply);
+
+                input = System.Console.ReadLine();
+            }
+            
+            
+            
+        }
+        private static Task<BroadcastReply> ProcessRequest(BroadcastRequest request, Broadcaster.BroadcasterClient client)
+        {
+            return client.RespondToRequestAsync(request).ResponseAsync;
+        }
+    }
+}
---
+++ GrpcBase.Service/Services/BroadcastService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcBase.Common;
+
+namespace GrpcBase.Service
+{
+    public class BroadcastService : Broadcaster.BroadcasterBase
+    {
+        public override Task<BroadcastReply> RespondToRequest(BroadcastRequest request, ServerCallContext context)
+        {
+            var user = context.GetHttpContext().User;
+            
+            return Task.FromResult(new BroadcastReply
+            {
+                Content = $"You wrote {request.Content} at {DateTime.Now.ToString("h:mm:ss tt")}"
+            });
+        }
+        
+        
+    }
+}
